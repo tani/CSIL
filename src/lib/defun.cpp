aa4366208b56ddd8c0fed4cf0b02622950d88859
TANIGUCHI Masaya <mew@cat.ovh>
2015-10-18 00:34:11 +0900

    bug fix

--- src/lib/defun.cpp
+++ src/lib/defun.cpp
@@ -1,12 +1,13 @@
-DEFMACRO(defun, obj, env, {
+DEFORM(defun, obj, env, {
   auto name = car(obj);
   auto args = car(cdr(obj));
   auto body = cdr(cdr(obj));
   defun(global_env, name->atom.symbol, [=](ObjPtr vars, Env env_1) -> ObjPtr {
     for (auto a = args, v = vars; v != nullptr && a != nullptr;
          a = cdr(a), v = cdr(v)) {
-      if (a->atom.symbol == "&rest") {
-        env_1.variables[car(a = cdr(a))->atom.symbol] = v;
+      if (car(a)->atom.symbol == "&rest") {
+        env_1.variables[car(cdr(a))->atom.symbol] = v;
+        break;
       }
       env_1.variables[car(a)->atom.symbol] = car(v);
     }
--- src/lib/defvar.cpp
+++ src/lib/defvar.cpp
@@ -1,4 +1,4 @@
-DEFMACRO(defvar, obj, env, {
+DEFORM(defvar, obj, env, {
   auto name = car(obj);
   auto var = car(cdr(obj));
   global_env.variables.at(name->atom.symbol) = eval(var, env);
